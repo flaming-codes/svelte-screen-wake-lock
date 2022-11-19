29e80934537debb040513b95ac52aaf5b4dd4c3e
Joe Kroese <joekroese@gmail.com>
2022-11-19 11:13:30 +0100

    update to @app/environment (#5)

--- src/lib/ScreenWakeLock.svelte
+++ src/lib/ScreenWakeLock.svelte
@@ -1,5 +1,5 @@
 <script lang="ts">
-  import { browser } from "$app/env";
+  import { browser } from "$app/environment";
 
   import type { WakeLockSentinel } from "src/types";
   import { onDestroy, onMount, createEventDispatcher } from "svelte";
